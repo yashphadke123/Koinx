ea36b076e9aab89b6af8d5b2a8829b7c2c944a26
Yash Phadke <f20211942@goa.bits-pilani.ac.in>
2024-09-04 23:47:44 +0530

    almost done with fundamentals

--- src/components/fundamentals.jsx
+++ src/components/fundamentals.jsx
@@ -2,7 +2,7 @@ import {useState,useEffect,React} from 'react'
 import styled from 'styled-components'
 
 function Fundamentals(props) {
-    const init_data = {"market_data":{"low_24h":{"usd":50000},"high_24h":{"usd":60000},"atl":{"usd":10000},"ath":{"usd":70000},"current_price":{"usd":50000}}}
+    const init_data = {"market_data":{"low_24h":{"usd":50000},"high_24h":{"usd":60000},"atl":{"usd":10000},"ath":{"usd":70000},"current_price":{"usd":50000},"total_volume":{"usd":50000},"market_cap":{"usd":30},"market_cap_rank":1}}
     const [coin,Setcoin] = useState(init_data)
     const api_key = process.env.REACT_APP_API_KEY
     useEffect(()=>{
@@ -27,16 +27,16 @@ function Fundamentals(props) {
         <div>Volume/Market Cap</div>
         <div>All-Time High</div>
         <div>All-Time Low</div>
-        <h3>{coin.market_data.current_price.usd}</h3>
-        {/* <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3>
-        <h3>{coin}</h3> */}
+        <h3>${coin.market_data.current_price.usd}</h3>
+        <h3>${coin.market_data.low_24h.usd}/${coin.market_data.high_24h.usd}</h3>
+        <h3>${coin.market_data.atl.usd}/${coin.market_data.ath.usd}</h3>
+        <h3>${coin.market_data.total_volume.usd}</h3>
+        <h3>#{coin.market_data.market_cap_rank}</h3>
+        <h3>${coin.market_data.market_cap.usd}</h3>
+        <h3>{(coin.market_data.market_cap.usd/3000000000000 *100).toFixed(3)}%</h3>
+        <h3>{(coin.market_data.total_volume.usd/coin.market_data.market_cap.usd).toFixed(3)}</h3>
+        <h3>${coin.market_data.ath.usd}</h3>
+        <h3>${coin.market_data.atl.usd}</h3>
         <h5></h5>
         <h5></h5>
         <div></div>
