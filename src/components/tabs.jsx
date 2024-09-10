e257a2f5e6936c90182a35eae86bbdf469d85b38
Yash Phadke <f20211942@goa.bits-pilani.ac.in>
2024-09-10 23:51:40 +0530

    final tabs

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -5,7 +5,8 @@ import OverView from './overview'
 import Fundamentals from './fundamentals'
 import News from './news'
 import Sentiment from './sentiment'
-import Team from './team'
+
+
 function Tabs() {
   const [toggle,setToggle] = useState(1)
   const toggle_Tabs = (index) =>{
@@ -13,12 +14,12 @@ function Tabs() {
   }
   return (
     <ul>  
-      <div onClick={()=>toggle_Tabs(1)} className={toggle === 1 ? 'active_tab':'tab'}>Overview
-        <div className={toggle === 1 ? 'active_content':'content'}>
+      <div onClick={()=>toggle_Tabs(1)} className={toggle === 1 ? 'active_tab':'tab'} id='overview'>Overview
+        <div className={toggle === 1 ? 'active_content':'content'} id='over'>
           <OverView id='bitcoin'></OverView>
         </div>
         </div>
-      <div onClick={()=>toggle_Tabs(2)} className={toggle === 2 ? 'active_tab':'tab'}>Fundamentals
+      <div onClick={()=>toggle_Tabs(2)} className={toggle === 2 ? 'active_tab':'tab'} id='funda'>Fundamentals
         <div className={toggle === 2 ? 'active_content':'content'}>
            <Fundamentals id='bitcoin'></Fundamentals>
         </div>
@@ -32,17 +33,6 @@ function Tabs() {
         <div className={toggle === 4 ? 'active_content':'content'}>
           <Sentiment id='bitcoin'></Sentiment>
           </div>
-      </div>
-      <div onClick={()=>toggle_Tabs(5)} className={toggle === 5 ? 'active_tab':'tab'}>Team
-        <div className={toggle === 5 ? 'active_content':'content'}>
-          <Team id='bitcoin'></Team>  
-        </div>
-      </div>
-      <div onClick={()=>toggle_Tabs(6)} className={toggle === 6 ? 'active_tab':'tab'}>Technicals
-        <div className={toggle === 6 ? 'active_content':'content'}>6</div>
-      </div>
-      <div onClick={()=>toggle_Tabs(7)} className={toggle === 7 ? 'active_tab':'tab'}>Tokenomics
-        <div className={toggle === 7 ? 'active_content':'content'}>7</div>
       </div>
     </ul> 
   )
